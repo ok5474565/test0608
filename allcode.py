8adac57dc3378fb6002bc1efe3bdc8e022bbb3fa
ok5474565 <13725981731@163.com>
2024-06-21 19:07:14 +0800

    Update allcode.py

--- allcode.py
+++ allcode.py
@@ -2,11 +2,14 @@ import streamlit as st
 
 # 导入子文件中的主函数
 from newspachong666 import main as news_crawl_main
+from app_abstract import main as summary_main  
+from app_bar2 import run as bar_chart_run  
+from app_worldcloud import run as wordcloud_run  
+from 99999999999999999 import main as text_analysis_main  # 假设主函数名为main
+from 8888888888888 import main as wordcloud_main  # 假设主函数名为main
+
 from count import main as count_main
 from ciyuntu import main as ciyuntu_main
-from app_bar2 import run as bar_chart_run  
-from app_abstract import main as summary_main  
-from app_worldcloud import run as wordcloud_run  
 from count3 import main as count3_main  # 新增：读取GBK CSV生成条形图
 from count1 import main as count1_main  # 新增：读取UTF-8 CSV生成条形图
 from ciyuntu3 import main as ciyuntu3_main  # 新增：读取GBK CSV生成词云图
@@ -21,11 +24,13 @@ st.sidebar.title("功能选择")
 # 创建下拉菜单供用户选择功能
 function_options = [
     "新闻爬虫",
-    "读取txt统计高频词生成条形图",
-    "读取txt生成词云图",
     "文本简要总结",
     "输入链接爬取内容统计词频条形图",
     "输入链接爬取内容生成词云图",
+    "在线文本分词与高频词统计小程序",  # 新增功能名称
+    "在线文本分词与词云图生成小程序",  # 新增功能名称
+    "读取txt统计高频词生成条形图",
+    "读取txt生成词云图",
     "读取GBK CSV生成条形图",  # 新增选项：count1.py
     "读取UTF-8 CSV生成条形图",  # 新增选项：count1.py
     "读取GBK CSV生成词云图",  # 新增选项：ciyuntu3.py
@@ -40,16 +45,20 @@ selected_option = st.sidebar.selectbox(
 # 根据用户选择的功能，调用相应的主函数
 if selected_option == "新闻爬虫":
     news_crawl_main()
-elif selected_option == "读取txt统计高频词生成条形图":
-    count_main()
-elif selected_option == "读取txt生成词云图":
-    ciyuntu_main()
 elif selected_option == "文本简要总结":
     summary_main()
 elif selected_option == "输入链接爬取内容统计词频条形图":
     bar_chart_run()
 elif selected_option == "输入链接爬取内容生成词云图":
     wordcloud_run()
+elif selected_option == "在线文本分词与高频词统计小程序":
+    text_analysis_main()
+elif selected_option == "在线文本分词与词云图生成小程序":
+    wordcloud_main()
+elif selected_option == "读取txt统计高频词生成条形图":
+    count_main()
+elif selected_option == "读取txt生成词云图":
+    ciyuntu_main()
 elif selected_option == "读取GBK CSV生成条形图":  # 新增选项的处理逻辑
     count3_main()
 elif selected_option == "读取UTF-8 CSV生成条形图":  # 新增选项的处理逻辑
