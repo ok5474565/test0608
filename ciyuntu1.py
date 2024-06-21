7897daa4251aea5bf5be7178be7550276154a678
ok5474565 <13725981731@163.com>
2024-06-21 10:42:01 +0800

    Update ciyuntu1.py

--- ciyuntu1.py
+++ ciyuntu1.py
@@ -5,7 +5,6 @@ from wordcloud import WordCloud
 import numpy as np
 from PIL import Image
 import pandas as pd
-import os
 
 # 辅助函数，用于清理标题中的非法字符
 def sanitize_word(word, illegal_chars):
@@ -17,45 +16,33 @@ def remove_stopwords(words, stopwords):
 
 # 生成词云图
 def generate_wordcloud(frequencies, font_path, width=800, height=600):
+    # 创建词云对象
     wc = WordCloud(
         font_path=font_path,
         background_color='white',
         max_words=200,
-        width=width,
-        height=height
+        width=width,  # 设置词云图的宽度
+        height=height  # 设置词云图的高度
     ).generate_from_frequencies(frequencies)
     
+    # 显示词云图
     image = wc.to_image()
-    st.image(image, use_column_width=True)
+    st.image(image, use_column_width=True)  # 根据需要调整图像大小
 
 # 主函数
 def main():
-    st.title("文本分词、高频词统计与词云图生成")
+    st.title("评论文本分词、高频词统计与词云图生成")
 
-    # 设置上传文件的按钮，接受TXT和CSV文件
-    file_type = st.selectbox(
-        "选择文件类型:",
-        ["TXT", "CSV"]
-    )
-    
-    if file_type == "TXT":
-        file_type = "txt"
-    else:
-        file_type = "csv"
-    
-    uploaded_file = st.file_uploader(f"请上传你的.{file_type}文件", type=[file_type])
+    # 设置上传文件的按钮，接受CSV文件
+    uploaded_file = st.file_uploader("请上传你的CSV文件", type=["csv"])
 
     if uploaded_file is not None:
-        file_content = uploaded_file.read().decode('utf-8')
-
-        if file_type == "csv":
-            # 读取CSV文件，假设没有列名，每一行是一个评论
-            data = pd.read_csv(uploaded_file, header=None)
-            comments = data.iloc[:, 0].astype(str)
-            file_content = ' '.join(comment for comment in comments)
+        # 读取CSV文件，假设没有列名，每一行是一个评论
+        data = pd.read_csv(uploaded_file, header=None)
+        comments = data.iloc[:, 0].astype(str)  # 确保是字符串格式
 
         # 使用jieba进行分词
-        words = jieba.lcut(file_content)
+        all_words = [word for comment in comments for word in jieba.lcut(comment)]
 
         # 读取停用词典文件
         with open('stopwords.txt', 'r', encoding='utf-8') as f:
@@ -65,7 +52,7 @@ def main():
         illegal_chars = [':','\"','|','/','\\\\','*','<','>','?']
 
         # 清理非法字符并去除停用词
-        sanitized_words = [sanitize_word(word, illegal_chars) for word in words if word]
+        sanitized_words = [sanitize_word(word, illegal_chars) for word in all_words if word]
         filtered_words = remove_stopwords(sanitized_words, stopwords)
 
         # 统计词频
@@ -78,7 +65,7 @@ def main():
         wordcloud_freq = {word: freq for word, freq in top_words}
 
         # 设置中文字体路径
-        font_path = 'simhei.ttf'  # 确保这个路径是正确的
+        font_path = 'simhei.ttf'  # 请确保这个路径是正确的
 
         # 生成并显示词云图
         generate_wordcloud(wordcloud_freq, font_path)
