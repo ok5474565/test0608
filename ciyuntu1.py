18a2835c5fcf3f1b2571f5ec83368075508962a5
ok5474565 <13725981731@163.com>
2024-06-21 11:00:58 +0800

    Update ciyuntu1.py

--- ciyuntu1.py
+++ ciyuntu1.py
@@ -3,10 +3,14 @@ import pandas as pd
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from PIL import ImageFont
+import jieba
 
 def create_wordcloud(text, font_path, max_words=200):
-    # 创建词云图，指定中文字体
-    wordcloud = WordCloud(width=800, height=400, background_color='white', max_words=max_words, font_path=font_path).generate(text)
+    # 使用jieba进行中文分词
+    cut_text = ' '.join(jieba.cut(text))
+    
+    # 创建词云图，指定中文字体和分辨率
+    wordcloud = WordCloud(width=1600, height=800, background_color='white', max_words=max_words, font_path=font_path).generate(cut_text)
     return wordcloud
 
 def main():
@@ -23,13 +27,13 @@ def main():
         text_data = ' '.join(str(v) for v in data.iloc[:, 0])
         
         # 指定中文字体路径，这里需要替换为你的字体文件路径
-        font_path = 'simhei.ttf'
+        font_path = 'path/to/your/chinese_font.ttf'
         
         # 创建词云图
         wordcloud = create_wordcloud(text_data, font_path)
         
         # 使用Streamlit的API显示词云图
-        fig, ax = plt.subplots()  # 创建一个新的图形和轴
+        fig, ax = plt.subplots(figsize=(20, 10))  # 创建一个新的图形和轴，并设置大小
         ax.imshow(wordcloud, interpolation='bilinear')
         ax.axis('off')  # 不显示轴
         st.pyplot(fig)  # 将图形传递给Streamlit显示
