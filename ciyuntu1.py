67cce20be4fac7bbd3bfb5ee82dbac9fae477f8e
ok5474565 <13725981731@163.com>
2024-06-21 11:15:51 +0800

    Update ciyuntu1.py

--- ciyuntu1.py
+++ ciyuntu1.py
@@ -1,25 +1,24 @@
 import streamlit as st
 import pandas as pd
+import jieba
+from collections import Counter
 from wordcloud import WordCloud
 from PIL import Image
 import numpy as np
 
-# 读取CSV文件并生成词云图的函数
-def generate_wordcloud_from_csv(csv_file, font_path):
-    # 读取CSV文件
-    data = pd.read_csv(csv_file)
-    
-    # 假设CSV文件中有列名为'text'的列，包含我们要生成词云的文本
-    text = ' '.join(str(v) for v in data['text'])
+# 定义生成词云图的函数
+def generate_wordcloud(text, font_path, max_words=200):
+    # 使用jieba进行分词
+    words = ' '.join(jieba.cut(text))
     
     # 创建词云对象
     wc = WordCloud(
         font_path=font_path,
         background_color='white',
-        max_words=200,
+        max_words=max_words,
         width=800,
-        height=400
-    ).generate(text)
+        height=600
+    ).generate(words)
     
     # 显示词云图
     image = wc.to_image()
@@ -33,11 +32,17 @@ def main():
     uploaded_file = st.file_uploader("请上传你的CSV文件", type=["csv"])
     
     if uploaded_file is not None:
-        # 读取文件内容
-        file_content = uploaded_file.read()
+        # 读取CSV文件内容
+        data = pd.read_csv(uploaded_file, header=None)  # 假设没有列名
         
-        # 将文件内容转换为Image对象
-        image = generate_wordcloud_from_csv(file_content, 'simhei.ttf')
+        # 将所有行的数据合并为一个字符串
+        all_text = ' '.join(str(row[0]) for row in data.values)  # 假设我们只关心第一列
+        
+        # 设置中文字体路径
+        font_path = 'simhei.ttf'  # 请确保这个路径是正确的
+        
+        # 生成词云图
+        image = generate_wordcloud(all_text, font_path)
         
         # 显示词云图
         st.image(image, use_column_width=True)
