81754af0e38e07f139df911f8d1a20f3f37d9b07
ok5474565 <13725981731@163.com>
2024-06-22 16:28:44 +0800

    Update spscore.py

--- spscore.py
+++ spscore.py
@@ -7,20 +7,22 @@ def main():
     st.title('S-P Chart and Analysis Tool')
 
     # 允许用户上传文件
-    uploaded_file = st.file_uploader("Please upload your score statistics table (.xlsx)", type=["xlsx"])
+    uploaded_file = st.file_uploader("Please upload your score statistics table (.xlsx)")
     
     if uploaded_file is not None:
         # 读取Excel文件
         data = pd.read_excel(uploaded_file)
 
-        # 学生姓名是第一列的值（跳过标题"对象")
-        students = data.iloc[:, 0].drop('对象').tolist()
+        # 学生姓名是第一列的值（跳过标题"对象"）
+        # 我们假设第一行是题目号码，第一列是学生姓名
+        students = data.iloc[:, 0]  # 获取第一列所有值
+        students = students[students != '对象']  # 排除标题"对象"
 
-        # 题目号码是第一行的值
-        problems = data.index[1:].tolist()
+        # 题目号码是第一行的值，排除第一列（学生姓名）
+        problems = data.index[:-1]  # 获取除最后一行的所有行名，假设最后一行是学生姓名
 
         # 构建DataFrame，排除标题"对象"
-        sp_df = pd.DataFrame(data[1:], columns=students)
+        sp_df = data.loc['P1':'P8']  # 假设列名是'P1'到'P8'
 
         # 计算每个学生的总分
         student_totals = sp_df.sum(axis=1)
@@ -32,15 +34,8 @@ def main():
         sorted_students = student_totals.sort_values(ascending=False)
         sorted_problems = problem_totals.sort_values(ascending=False)
 
-        # 排序索引
-        sorted_students_index = sorted_students.index
-        sorted_problems_index = sorted_problems.index
-
-        # 创建排序后的S-P表格
-        sorted_sp_df = sp_df.loc[sorted_students_index, sorted_problems_index]
-
-        # 将学生姓名添加到排序后的DataFrame
-        sorted_sp_df.index = students
+        # 根据总分排序S-P表
+        sorted_sp_df = sp_df.loc[sorted_students.index, sorted_problems.index]
 
         # 显示S-P表格
         st.write("S-P Table:")
